9486f2590f187d078a97e3c042a8bfa9b857e24c
Joshua Hamilton <hamiltonjoshuadavid@gmail.com>
2025-09-24 19:47:27 -0700

    small note

--- app.py
+++ app.py
@@ -235,4 +235,4 @@ st.markdown(
     "**Desired Retention** is the rate that determines when a learner should review a card. A card becomes due as soon as it's retrievability falls below the desired retention. Higher desired retention rates generally lead to more reviews and shorter spaces between reviews."
 )
 
-st.markdown("(And yes, this app is clunky. It's a streamlit app)")
+st.markdown("(Apologies to any mobile users! Streamlit apps are pretty clunky and are not responsive...)")
